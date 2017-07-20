0faad64183c37d591d114b4c91957a889227a7d8
dale <dale.guo@mullenloweprofero.com>
2017-07-20 16:31:56 +0800

    ready

---
+++ README.md
@@ -0,0 +1,15 @@
+README.md
+=====
+
+1. install docker
+
+2. install docker-compose
+
+3. replace your code git rep url && branch docker_yourproject.
+
+4. papare sql if is not a new project. 
+
+5. chmod +x run.sh && ./run.sh yourproject http://github.com/yourname/yourproject.git
+
+6. import your sql & old file though run.bash doc.
+
--- run.sh
+++ run.sh
@@ -1,16 +1,46 @@
-#! /bin/bash
-# if no git 
-cd ./code
-if [ -d .git ]; then
-  	sudo git checkout .
-	sudo git pull origin master
-else
-	cd ../
-	rm ./code -rf
-	git clone https://github.com/guoxiangke/api.git ./code
+	#! /bin/bash
+	if [ -z "$1" ]
+	then
+	     ProjectName="api"
+	else
+		ProjectName=$1
+	fi
+	if [ -z "$1" ]
+	then
+	     GitUrl="https://github.com/guoxiangke/api.git"
+	else
+		GitUrl=$2
+	fi
+	echo your github url $GitUrl
+	echo Your ProjectName is : $ProjectName
 
-fi;
-cd ../
-# sudo chmod 755 -R ./code
-# sudo chown -R www-data:www-data ./code
-docker-compose -f ./docker-compose.yml up -d --build --remove-orphans
+	Branch="docker_"
+	GitBranch=$Branch$ProjectName
+
+	echo your github Branch must be $GitBranch
+	cd ./code
+	if [ -d .git ]; then
+	  	sudo git checkout .
+	  	sudo git fetch
+	  	sudo git checkout $GitBranch
+		sudo git pull origin $GitBranch
+	else
+		cd ../
+		rm ./code -rf
+		git clone $GitUrl ./code
+
+	fi;
+
+	cd ../
+	# sudo chmod 755 -R ./code
+	# sudo chown -R www-data:www-data ./code
+	docker-compose -p $ProjectName -f ./docker-compose.yml up -d  --build --remove-orphans
+
+	###import databses example!!!!
+	# docker exec -it api_db_1 bash
+	# mysql -udrupal -pdrupal
+	# use drupal;
+	# set autocommit =1;
+	# source /sql/api.sql;
+
+	###import old files for drupal example!!!!
